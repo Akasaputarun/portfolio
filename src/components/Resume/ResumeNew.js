695f4803f59cac877a3901f47a977a1d0087a94a
Akasaputarun <tarun.akasapu@gmail.com>
2025-02-27 15:50:56 -0600

    Updated resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -3,7 +3,7 @@ import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 //import pdf from "../../Assets/../Assets/Narasimha Naidu Kommi Resume.pdf";
-import pdf from "../../Assets/TarunAkasapuResume.pdf";
+import pdf from "../../Assets/Tarun Akasapu.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
